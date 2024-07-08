44fcec0aff189b5849a5dd0a42082c2d1cb08d7d
Csanád Bakos <bakoscsanad@gmail.com>
2024-07-08 20:44:38 +0200

    make partial gauss more modular

--- src/optimizers/partial_gauss.py
+++ src/optimizers/partial_gauss.py
@@ -13,11 +13,23 @@ def partial_row_reduce(a: np.array, k: int) -> np.array:
 
     # Elimination process
     for i in range(k):  # first k rows
-        a_i = a[i, :]
-        a_ii = a_i[i]
-        for j in range(i + 1, n):  # columns from pivot column i
-            m = a[j, i] / a_ii
-            a[j, :] = a[j, :] - m * a_i
+        a = partial_row_reduce_step(a, i, n)
+    return a
+
+
+def partial_row_reduce_step(a: np.array, i: int, n: int) -> np.array:
+    """
+
+    :param a: matrix to reduce
+    :param i: index of variable to reduce
+    :param n: total number of variables == number of rows
+    :return: a with ith variable reduced
+    """
+    a_i = a[i, :]
+    a_ii = a_i[i]
+    for j in range(i + 1, n):  # columns from pivot column i
+        m = a[j, i] / a_ii
+        a[j, :] = a[j, :] - m * a_i
     return a
 
 
