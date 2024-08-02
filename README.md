4818384754de6373dd802ee42a2348a6be2e8833
Csanád Bakos <bakoscsanad@gmail.com>
2024-08-02 22:47:51 +0200

    add special matrices to rank assessment

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sparse_tt_decomp_opt
+
+
+### Install scikit-tt by:
+```pip install git+https://github.com/PGelss/scikit_tt```
+ 
--- src/experiments/rank_assessment.py
+++ src/experiments/rank_assessment.py
@@ -1,21 +1,37 @@
 import numpy as np
 from scipy.io import mmread
-import numpy.linalg as la
+import numpy.linalg as nla
+import scipy.linalg as sla
 import scikit_tt as stt
 
 from optimizers.tile_size import prime_factors
 
 
+def get_matrix(matrix_name: str, n: int = 2000) -> np.ndarray:
+    x = np.arange(n)
+    if matrix_name == "hilbert":
+        dense_a = sla.hilbert(n)
+    elif matrix_name == "vandermonde":
+        dense_a = np.vander(x)
+    elif matrix_name == "hankel":
+        dense_a = sla.hankel(x)
+    elif matrix_name == "toeplitz":
+        dense_a = sla.toeplitz(x)
+    else:
+        path = "../../data/{}/{}.mtx".format(matrix_name, matrix_name)
+        a = mmread(path)  # reads to coo_matrix format
+        dense_a = a.toarray()
+    return dense_a
+
+
 def get_ranks_for_matrix(matrix_name: str):
-    path = "../../data/{}/{}.mtx".format(matrix_name, matrix_name)
-    a = mmread(path)  # reads to coo_matrix format
-    dense_a = a.toarray()
+    dense_a = get_matrix(matrix_name)
     tol1 = 1e-3
     tol2 = 1e-10
     print("matrix: {}".format(matrix_name))
-    print("rank: ", la.matrix_rank(dense_a))
-    print("numerical rank, tol {}: {}".format(tol1, la.matrix_rank(dense_a, tol=tol1)))
-    print("numerical rank, tol {}: {}".format(tol2, la.matrix_rank(dense_a, tol=tol2)))
+    print("rank: ", nla.matrix_rank(dense_a))
+    print("numerical rank, tol {}: {}".format(tol1, nla.matrix_rank(dense_a, tol=tol1)))
+    print("numerical rank, tol {}: {}".format(tol2, nla.matrix_rank(dense_a, tol=tol2)))
 
     mode_sizes = prime_factors(dense_a.shape[0])
     mode_sizes = mode_sizes + mode_sizes
@@ -32,6 +48,6 @@ def get_ranks_for_matrix(matrix_name: str):
 
 
 if __name__ == '__main__':
-    matrix_names = ["ex10", "ex3", "ex10hs", "ex13"]
+    matrix_names = ["ex10", "ex3", "ex10hs", "ex13", "hilbert", "vandermonde", "hankel", "toeplitz"]
     for matrix_name in matrix_names:
         get_ranks_for_matrix(matrix_name)
